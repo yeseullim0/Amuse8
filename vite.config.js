0db15494a131c12c8a70bd796e23d74ab5ea4bbe
lim Ye seul <dlagudrns@naver.com>
2025-01-14 16:28:33 +0900

    images error

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png'],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
   plugins: [react()],
 })
